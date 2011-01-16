c8722e664f1d9dae323ffaafb25baa0d72977dd6
Mattias Bogeblad <mattias.bogeblad@gmail.com>
2011-01-16 22:38:25 +0100

    Removed debug

--- src/java/org/infoglue/cms/webservices/RemoteFormServiceImpl.java
+++ src/java/org/infoglue/cms/webservices/RemoteFormServiceImpl.java
@@ -224,8 +224,8 @@ public class RemoteFormServiceImpl extends RemoteInfoGlueService
 							newAsset.setFileName(remoteAttachment.getFileName());
 							newAsset.setFileSize(new Integer(new Long(remoteAttachment.getBytes().length).intValue()));
 							byte[] bytes = remoteAttachment.getBytes();
-							for(int i=0; i<bytes.length; i++)
-								System.out.print(bytes[i]);
+							//for(int i=0; i<bytes.length; i++)
+							//	System.out.print(bytes[i]);
 							InputStream is = new ByteArrayInputStream(bytes);
 		
 							formEntryController.createAsset(newAsset, newFormEntry, is, newFormEntry.getId(), principal, db2);

fc67b194d2441a899c924c47ad9f85e15ade1f74
Mattias Bogeblad <mattias.bogeblad@gmail.com>
2011-04-02 21:12:16 +0200

    dddd

--- src/java/org/infoglue/deliver/controllers/kernel/impl/simple/RepositoryDeliveryController.java
+++ src/java/org/infoglue/deliver/controllers/kernel/impl/simple/RepositoryDeliveryController.java
@@ -189,7 +189,8 @@ public class RepositoryDeliveryController extends BaseDeliveryController
                 	index = dnsName.indexOf("live=,");
                 	indexMode = dnsName.indexOf("live=");
                 }
-
+                
+                System.out.println("" + index + ":" + indexMode + ":" + dnsName + " for operationMode:" + CmsPropertyHandler.getOperatingMode());
             	if(index == -1 && indexMode == -1 && dnsName.indexOf("=") > -1)
             	{
             		System.out.println("Skipping this name [" + dnsName + "] as it was not a dnsName targeted toward this mode.");

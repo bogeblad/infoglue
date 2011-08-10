f66477c8db6eca8d8d3599a51167e086593d31b6
Mattias Bogeblad <mattias.bogeblad@gmail.com>
2011-08-10 23:09:18 +0200

    Improved logging by adding a Download logfile option

--- src/java/org/infoglue/cms/applications/managementtool/actions/ViewLoggingAction.java
+++ src/java/org/infoglue/cms/applications/managementtool/actions/ViewLoggingAction.java
@@ -23,7 +23,12 @@
 
 package org.infoglue.cms.applications.managementtool.actions;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -31,12 +36,14 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.infoglue.cms.applications.common.VisualFormatter;
 import org.infoglue.cms.applications.common.actions.InfoGlueAbstractAction;
 import org.infoglue.cms.controllers.kernel.impl.simple.ServerNodeController;
+import org.infoglue.cms.exception.SystemException;
 import org.infoglue.cms.io.FileHelper;
 import org.infoglue.cms.util.CmsPropertyHandler;
 import org.infoglue.cms.util.sorters.FileComparator;
@@ -59,6 +66,94 @@ public class ViewLoggingAction extends InfoGlueAbstractAction
 	private List logFiles = new ArrayList();
 	private String logFileName = null;
 
+	public String doDownloadFile() throws Exception
+	{
+		boolean allowAccess = true;
+        if(!ServerNodeController.getController().getIsIPAllowed(getRequest()))
+        {
+        	java.security.Principal principal = (java.security.Principal)getHttpSession().getAttribute("infogluePrincipal");
+    		if(principal == null)
+    			principal = getInfoGluePrincipal();
+    		
+    		if(principal != null && org.infoglue.cms.controllers.kernel.impl.simple.AccessRightController.getController().getIsPrincipalAuthorized((org.infoglue.cms.security.InfoGluePrincipal)principal, "ViewApplicationState.Read", false, true))
+    		{
+    			allowAccess = true;
+    		}
+    		else
+    		{
+    			allowAccess = false;
+    			logger.error("A client with IP " + getRequest().getRemoteAddr() + " was denied access to the download action. Could be a hack attempt or you have just not configured the allowed IP-addresses correct.");
+               	this.getResponse().setContentType("text/plain");
+                this.getResponse().setStatus(HttpServletResponse.SC_FORBIDDEN);
+                this.getResponse().getWriter().println("You have no access to this view - talk to your administrator if you should. Try go through the ViewApplicationState.action if you have an account that have access.");
+                return NONE;
+    		}
+        }
+    	
+    	if(logFileName != null && !logFileName.equals(""))
+    	{
+        	String catalinaBase = System.getProperty("catalina.base");
+        	if(catalinaBase == null || catalinaBase.equals(""))
+        	{	
+        		catalinaBase = CmsPropertyHandler.getContextRootPath().substring(0, CmsPropertyHandler.getContextRootPath().lastIndexOf("/"));
+        	}
+        	catalinaBase = catalinaBase + File.separator + "logs";
+
+        	String velocityLog = CmsPropertyHandler.getContextRootPath() + File.separator + "velocity.log";
+
+    		List fileList = Arrays.asList(new File(catalinaBase).listFiles());
+    		logFiles.addAll(fileList);
+    		
+    		List debugFileList = Arrays.asList(new File(CmsPropertyHandler.getContextRootPath() + File.separator + "logs").listFiles());
+    		logFiles.addAll(debugFileList);
+    		
+    		File velocityLogFile = new File(velocityLog);
+    		if(velocityLogFile.exists())
+    		{
+    			logFiles.add(velocityLogFile);
+    		}
+
+    		if(logFiles != null && logFiles.size() > 0)
+    			Collections.sort(logFiles, Collections.reverseOrder(new FileComparator("lastModified")));
+
+    		File file = new File(logFileName);
+    		if(file.exists() && logFiles.contains(file))
+    		{
+	    		HttpServletResponse response = this.getResponse();
+	    		response.addHeader("Content-Type", "application/force-download");
+	    		response.addHeader("Content-Disposition", "attachment; filename=\"downloadedLog.txt\"");
+	    		
+		        // print some html
+		        ServletOutputStream out = response.getOutputStream();
+		        
+		        // print the file
+		        InputStream in = null;
+		        try 
+		        {
+		            in = new BufferedInputStream(new FileInputStream(file));
+		            int ch;
+		            while ((ch = in.read()) !=-1) 
+		            {
+		                out.print((char)ch);
+		            }
+		        }
+		        finally 
+		        {
+		            if (in != null) in.close();  // very important
+		        }
+    		}
+    		else
+    		{
+    			this.getResponse().setContentType("text/plain");
+                this.getResponse().setStatus(HttpServletResponse.SC_FORBIDDEN);
+                this.getResponse().getWriter().println("No file or no access to file:" + logFileName);
+                return NONE; 
+    		}
+    	}
+    	
+		return NONE;
+	}
+	
     public String doExecute() throws Exception
     {
     	boolean allowAccess = true;

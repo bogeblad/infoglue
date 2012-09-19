75dd913c330e8c8d14998a98240bd0e105725668
Johan Dahlgren <johan.dahlgren@knowit.se>
2012-09-19 12:44:41 +0200

    Mergat in Mattias förra fixar.

--- src/java/org/infoglue/cms/controllers/kernel/impl/simple/LuceneController.java
+++ src/java/org/infoglue/cms/controllers/kernel/impl/simple/LuceneController.java
@@ -596,6 +596,7 @@ public class LuceneController extends BaseController implements NotificationList
 		logger.warn("INDEXING ALL - correct?");
 		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 		
+		stopIndexing.set(false);
 		logger.info("------------------------------Got indexAll directive....");
 		if (indexingInitialized.compareAndSet(false, true)) 
 		{
@@ -642,12 +643,13 @@ public class LuceneController extends BaseController implements NotificationList
 						registerIndexAllProcessOngoing(newLastContentVersionId, null, languageVO.getId());
 						//previousIndexAllLastContentVersionId = newLastContentVersionId;
 						RequestAnalyser.getRequestAnalyser().registerComponentStatistics("getNotificationMessages 2", t.getElapsedTime());
+						logger.info("newLastContentVersionId " + newLastContentVersionId);
 					}
 				}
 				
 				languageVOList = LanguageController.getController().getLanguageVOList();
 				languageVOListIterator = languageVOList.iterator();
-				while(languageVOListIterator.hasNext())
+				outer:while(languageVOListIterator.hasNext())
 				{
 					LanguageVO languageVO = (LanguageVO)languageVOListIterator.next();
 					
@@ -699,7 +701,6 @@ public class LuceneController extends BaseController implements NotificationList
 			logger.warn("-------------------: Allready running index all...");
 			return false;
 		}
-		*/
 		return true;
 	}
 	
@@ -1214,7 +1215,7 @@ public class LuceneController extends BaseController implements NotificationList
 		}
 		if(logger.isInfoEnabled())
 			t2.printElapsedTime("All indexing took");
-		*/
+
 		return true;
 	}
 	
@@ -1530,7 +1531,6 @@ public class LuceneController extends BaseController implements NotificationList
 	
 	private void indexInformation(NotificationMessage notificationMessage, IndexWriter writer, List<NotificationMessage> internalMessageList, Boolean forceVersionIndexing, Database db)
 	{
-		/*
     	Timer t = new Timer();
 
 		try 
@@ -1575,7 +1575,6 @@ public class LuceneController extends BaseController implements NotificationList
 	    {
 			logger.error("Error indexing:" + e.getMessage(), e);
 	    }
-	    */
 	}
 	
 	
@@ -1614,7 +1613,7 @@ public class LuceneController extends BaseController implements NotificationList
 			
 			try
 			{			
-				//////////ANTAGLIGEN ON�DIGT MED MEDIUM h�r
+				//////////ANTAGLIGEN ONDIGT MED MEDIUM hr
 				MediumDigitalAssetImpl asset = (MediumDigitalAssetImpl)DigitalAssetController.getMediumDigitalAssetWithIdReadOnly((Integer)notificationMessage.getObjectId(), db2);
 				RequestAnalyser.getRequestAnalyser().registerComponentStatistics("getMediumDigitalAssetWithIdReadOnly", t.getElapsedTime());
 				Collection contentVersions = asset.getContentVersions();
@@ -1654,7 +1653,7 @@ public class LuceneController extends BaseController implements NotificationList
 			//Deleting all info based on content
 			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_34);
 			logger.info("Deleting all info on:" + siteNodeId);
-			//TODO - Fixa s� inte assets tas med h�r....
+			//TODO - Fixa s inte assets tas med hr....
 		    Query query = new QueryParser(Version.LUCENE_34, "siteNodeId", analyzer).parse("" + siteNodeId); 
 			writer.deleteDocuments(query);
 			//End
@@ -1675,7 +1674,7 @@ public class LuceneController extends BaseController implements NotificationList
 			//Deleting all info based on content
 			Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_34);
 			logger.info("Deleting all info on:" + contentId);
-			//TODO - Fixa s� inte assets tas med h�r....
+			//TODO - Fixa s inte assets tas med hr....
 			
 			String[] fields = new String[]{"isAsset","contentId"};
 			String[] queries = new String[]{"true","" + contentId};

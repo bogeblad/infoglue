51955935a0e647131fd7c94d5d6bb44c64a4fbd4
Mattias Bogeblad <mattiasbogeblad@Mattiass-MacBook-Pro.local>
2013-09-13 08:04:46 +0200

    Quick fix for lucene demo

--- src/java/org/infoglue/cms/controllers/kernel/impl/simple/LuceneController.java
+++ src/java/org/infoglue/cms/controllers/kernel/impl/simple/LuceneController.java
@@ -1232,6 +1232,9 @@ public class LuceneController extends BaseController implements NotificationList
 
 	public boolean indexIncremental(Integer lastCommitedContentVersionId, Date lastCommitedDateTime) throws Exception
 	{
+		if(true)
+			return false;
+		
 		if(!CmsPropertyHandler.getInternalSearchEngine().equalsIgnoreCase("lucene"))
 			return false;
 

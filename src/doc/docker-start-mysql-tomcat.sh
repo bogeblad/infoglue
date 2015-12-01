03d967f46aa63d3d0ecd26bfc53ae4c94d01e7f6
Mattias Bogeblad <mattias.bogeblad@gmail.com>
2015-12-01 20:51:19 +0100

    new pass

--- src/doc/docker-start-mysql-tomcat.sh
+++ src/doc/docker-start-mysql-tomcat.sh
@@ -7,9 +7,9 @@ set -e
 
 sleep 10
 
-mysql -h 127.0.0.1 -P 3306 -u root -e "CREATE DATABASE IF NOT EXISTS infoglue"
-mysql -h 127.0.0.1 -P 3306 -u root -e "GRANT ALL ON your_db.* to 'infoglue'@'%' IDENTIFIED BY 'changeit'"
-mysql -h 127.0.0.1 -P 3306 -u root -e "FLUSH PRIVILEGES"
+mysql -h 127.0.0.1 -P 3306 -u root -pchangeit -e "CREATE DATABASE IF NOT EXISTS infoglue"
+mysql -h 127.0.0.1 -P 3306 -u root -pchangeit -e "GRANT ALL ON your_db.* to 'infoglue'@'%' IDENTIFIED BY 'changeit'"
+mysql -h 127.0.0.1 -P 3306 -u root -pchangeit -e "FLUSH PRIVILEGES"
 
 echo "Created database"
 

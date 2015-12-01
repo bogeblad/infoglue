2682bcdfd902dff345ed2a51793155391911aa4c
Mattias Bogeblad <mattias.bogeblad@gmail.com>
2015-12-01 17:04:44 +0100

    memory

--- src/doc/docker-start-mysql-tomcat.sh
+++ src/doc/docker-start-mysql-tomcat.sh
@@ -11,5 +11,7 @@ mysql -h 127.0.0.1 -P 3306 -u root -e "CREATE DATABASE IF NOT EXISTS infoglue"
 mysql -h 127.0.0.1 -P 3306 -u root -e "GRANT ALL ON your_db.* to 'infoglue'@'%' IDENTIFIED BY 'changeit'"
 mysql -h 127.0.0.1 -P 3306 -u root -e "FLUSH PRIVILEGES"
 
+echo "Created database"
 
+export JAVA_OPTS="-Dfile.encoding=ISO-8859-1 -XX:MaxPermSize=256m -Xms128M -Xmx1024M"
 catalina.sh run

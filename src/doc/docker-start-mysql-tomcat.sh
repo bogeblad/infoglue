5882bdd79786d9ed1e82ad9b024d1c4b1afea2d1
Mattias Bogeblad <mattias.bogeblad@gmail.com>
2015-12-02 00:13:26 +0100

    Fix for docker

--- src/doc/docker-start-mysql-tomcat.sh
+++ src/doc/docker-start-mysql-tomcat.sh
@@ -14,4 +14,11 @@ mysql -h 127.0.0.1 -P 3306 -u root -pchangeit -e "FLUSH PRIVILEGES"
 echo "Created database"
 
 export JAVA_OPTS="-Dfile.encoding=ISO-8859-1 -XX:MaxPermSize=256m -Xms128M -Xmx1024M"
-catalina.sh run &
+catalina.sh run
+
+sleep 120
+
+catalina.sh stop
+sleep 30
+
+catalina.sh run

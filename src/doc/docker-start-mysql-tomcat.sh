1b3b7672ebc797d0a6e3a7bbeb38e0e7699ada5a
Mattias Bogeblad <mattias.bogeblad@gmail.com>
2015-12-02 09:26:14 +0100

    flags

--- src/doc/docker-start-mysql-tomcat.sh
+++ src/doc/docker-start-mysql-tomcat.sh
@@ -3,13 +3,7 @@
 set -m
 set -e
 
-sed '/nice            = 0/r lower_case_table_names=2' /usr/my.cnf
-sed '/nice            = 0/r lower_case_table_names=2' /etc/mysql/my.cnf
-
-sed '/nice            = 0/r max_allowed_packet=32M' /usr/my.cnf
-sed '/nice            = 0/r max_allowed_packet=32M' /etc/mysql/my.cnf
-
-/usr/bin/mysqld_safe &
+/usr/bin/mysqld_safe --lower_case_table_names=1 --max_allowed_packet=32M &
 
 sleep 10
 
@@ -22,11 +16,4 @@ mysql -h 127.0.0.1 -P 3306 -u root -pchangeit infoglue < /tmp/infoglue3.sql
 echo "Created database"
 
 export JAVA_OPTS="-Dfile.encoding=ISO-8859-1 -XX:MaxPermSize=256m -Xms128M -Xmx1024M"
-catalina.sh run
-
-sleep 120
-
-catalina.sh stop
-sleep 30
-
 catalina.sh run

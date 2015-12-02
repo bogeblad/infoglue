6a618086efb744aaf0320bcc779a0842571506fe
Mattias Bogeblad <mattias.bogeblad@gmail.com>
2015-12-02 08:37:10 +0100

    Imports infoglue3 in docker

--- src/doc/docker-start-mysql-tomcat.sh
+++ src/doc/docker-start-mysql-tomcat.sh
@@ -11,6 +11,8 @@ mysql -h 127.0.0.1 -P 3306 -u root -pchangeit -e "CREATE DATABASE IF NOT EXISTS 
 mysql -h 127.0.0.1 -P 3306 -u root -pchangeit -e "GRANT ALL ON your_db.* to 'infoglue'@'%' IDENTIFIED BY 'changeit'"
 mysql -h 127.0.0.1 -P 3306 -u root -pchangeit -e "FLUSH PRIVILEGES"
 
+mysql -h 127.0.0.1 -P 3306 -u root -pchangeit infoglue < /tmp/infoglue3.sql
+
 echo "Created database"
 
 export JAVA_OPTS="-Dfile.encoding=ISO-8859-1 -XX:MaxPermSize=256m -Xms128M -Xmx1024M"
